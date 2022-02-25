1dca3b78d689d5d4b5b778ffb37e7f98266784c6
Jorge Mollericon <jmollericon@att.gob.bo>
2022-02-25 11:26:32 -0400

    base modules implementation, and implementation of auth controller and service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signin() {
+    return { msg: 'signin' };
+  }
+  signup() {
+    return { msg: 'signup' };
+  }
+}
