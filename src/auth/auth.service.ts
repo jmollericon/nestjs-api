be885977833333696e287bccb2a43ecc21e60d28
Jorge Mollericon <jmollericon@att.gob.bo>
2022-02-25 16:09:41 -0400

    use authDto, class-validators, pipes and logic Sign Up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'signin' };
+
+  async signup(authDto: AuthDto) {
+    // generate thes password hash
+    const hash = await argon.hash(authDto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: authDto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
-  signup() {
-    return { msg: 'signup' };
+
+  async signin() {
+    return { msg: 'signin' };
   }
 }
